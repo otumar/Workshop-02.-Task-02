47a045fa32a390e2e0aedd5799f5df61ef1db527
«Тумар <otumar@yandex.ru>
2022-06-14 00:56:38 +0300

    Task02

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+int number = new Random().Next(100,999);
+System.Console.WriteLine(number);
+
+int number1 = number / 100;
+int number3 = number % 10;
+
+System.Console.WriteLine ($"{number1}{number3}");
